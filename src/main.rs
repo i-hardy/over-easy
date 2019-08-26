d73ca8c50e8fcf15751cbb8d7fdc0057042f4006
Imogen Hardy <imogen.hardy@gmail.com>
2019-08-26 18:52:45 +0100

    Working Egg parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,4 @@
+#[macro_use] extern crate lazy_static;
 extern crate regex;
 mod parser;
 use parser::parse;
@@ -5,7 +6,7 @@ use std::env;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
-  let program = args.last().unwrap();
-  let tree = parse::parse_expression(program);
+  let program = args.last().unwrap().trim();
+  let tree = parse::parse(program);
   println!("{:?}", tree);
 }
--- src/parser.rs
+++ src/parser.rs
@@ -1,5 +1,5 @@
 pub mod parse {
-  use regex::Regex;
+  use regex::Regex;  
   
   #[derive(Debug)]
   pub enum ASTType {
@@ -22,6 +22,11 @@ pub mod parse {
       ast_type: ASTType,
       name: String,
     },
+    ASTApply {
+      ast_type: ASTType,
+      operator: Box<ASTExpression>,
+      args: Vec<ASTExpression>,
+    }
   }
 
   #[derive(Debug)]
@@ -30,33 +35,55 @@ pub mod parse {
     rest: Option<String>
   }
 
-  pub fn parse_apply<'a>(expr: ASTExpression, program: &'a str) -> AST {
-    if &program.chars().next().unwrap() != &'(' {
+  fn parse_apply<'a>(expr: ASTExpression, program: &'a str) -> AST {
+    if &program.chars().next().unwrap_or(Default::default()) != &'(' {
       return AST { expr, rest: Some(program.to_string()) };
     }
-    return AST { expr, rest: Some(program.to_string()) };
+    let mut to_parse: String = program[1..].to_string();
+    let mut args: Vec<ASTExpression> = Vec::new();
+    while &to_parse.chars().next().unwrap() != &')' {
+      let arg = parse_expression(&to_parse);
+      args.push(arg.expr);
+      to_parse = arg.rest.unwrap().trim_start().to_string();
+      let next_char = &to_parse.chars().next().unwrap();
+      if next_char == &',' {
+        to_parse = to_parse[1..].trim_start().to_string();
+      } else if next_char != &')' {      
+        panic!("Expected ',' or ')', received {}", next_char);
+      }
+    }
+    let new_expr = ASTExpression::ASTApply { ast_type: ASTType::Apply, operator: Box::new(expr), args };
+    return parse_apply(new_expr, &to_parse[1..]);
   }
 
-  pub fn parse_expression<'a>(program: &'a str) -> AST {
-    let num_regex = Regex::new(r"^\d+\b").unwrap();
-    let str_regex = Regex::new(r"^'([^']*)'").unwrap();
-    let word_regex = Regex::new("^[^\\s(),#\"]+").unwrap();
-    if num_regex.is_match(program) {
-      let value = num_regex.find(program).unwrap().as_str();
+  fn parse_expression<'a>(program: &'a str) -> AST {
+    lazy_static! {
+      static ref NUMBER: Regex = Regex::new(r"^\d+\b").unwrap();
+      static ref STRING: Regex = Regex::new(r"^'([^']*)'").unwrap();
+      static ref WORD: Regex = Regex::new("^[^\\s(),#\"]+").unwrap();
+    }
+    let to_parse = program.trim_start();
+    if NUMBER.is_match(to_parse) {
+      let value = NUMBER.find(to_parse).unwrap().as_str();
       let int_value = i32::from_str_radix(value, 10).unwrap();
       let expression = ASTExpression::ASTNumber { ast_type: ASTType::Value, value: int_value };
-      return parse_apply(expression, &program[value.len()..]);
-    } else if str_regex.is_match(program) {
-      let caps = str_regex.captures(program).unwrap();
+      return parse_apply(expression, &to_parse[value.len()..]);
+    } else if STRING.is_match(to_parse) {
+      let caps = STRING.captures(to_parse).unwrap();
+      let full_str = caps.get(0).unwrap().as_str();
       let value = caps.get(caps.len() - 1).unwrap().as_str();
       let expression = ASTExpression::ASTString { ast_type: ASTType::Value, value: value.to_string() };
-      return parse_apply(expression, &program[value.len()..]);
-
-    } else if word_regex.is_match(program) {
-      let value = word_regex.find(program).unwrap().as_str();
+      return parse_apply(expression, &to_parse[full_str.len()..]);
+    } else if WORD.is_match(to_parse) {
+      let value = WORD.find(to_parse).unwrap().as_str();
       let expression = ASTExpression::ASTWord { ast_type: ASTType::Word, name: value.to_string() };
-      return parse_apply(expression, &program[value.len()..]);
+      return parse_apply(expression, &to_parse[value.len()..]);
     }
     panic!("Unexpected syntax: {}", program);
+  }
+
+  pub fn parse<'a>(program: &'a str) -> ASTExpression {
+    let parsed = parse_expression(program);
+    return parsed.expr;
   }
 }
